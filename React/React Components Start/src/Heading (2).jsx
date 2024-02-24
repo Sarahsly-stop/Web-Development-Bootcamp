e2d5d2a644707836a3fba2a5896cbe10cff388c6
Walters <Sarah.Walters@bain.com>
2024-02-24 16:43:36 -0500

    more basic react practice using codesandbox

---
+++ React/React Components Start/src/Heading (2).jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX is for componets
+function Heading() {
+  return <h1> My Favorite Foods </h1>;
+}
+
+export default Heading;
---
+++ React/React Components Start/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
