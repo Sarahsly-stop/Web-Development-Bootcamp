10dd25f2ac489ab794e2e056517b1496648453eb
Walters <Sarah.Walters@bain.com>
2024-03-02 22:32:19 -0500

    forms

---
+++ React/React Forms/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText} </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
